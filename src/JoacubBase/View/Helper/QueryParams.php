8dbfee8801ac71679601bc7781b6fe560c9e98bc
Johan <joacub@gmail.com>
2013-08-30 13:15:45 +0200

    mejora en query params

--- src/JoacubBase/View/Helper/QueryParams.php
+++ src/JoacubBase/View/Helper/QueryParams.php
@@ -1,44 +1,40 @@
 <?php
-
 namespace JoacubBase\View\Helper;
-
 use Zend\View\Helper\AbstractHelper;
 use Zend\EventManager\StaticEventManager;
 use Zend\Mvc\Router\Http\Query;
+use Nette\Diagnostics\Debugger;
 
 class QueryParams extends AbstractHelper
 {
-    /**
-     * @param array $params
-     * @param bool $reset
-     * @return string
-     */
-    public function __invoke($params = array(), $reuseMatchedParams = true, $resetCurrentParams = false)
-    {
-        $currentParams = array();
 
-        if (!$resetCurrentParams) {
-            $queryString = $_SERVER['QUERY_STRING'];
-
-            $currentParamPairs = explode('&', $queryString);
-
-            if (!empty($currentParamPairs[0])) {
-                foreach ($currentParamPairs as $pair) {
-                    $data = explode('=', $pair);
-                    $currentParams[$data[0]] = urldecode($data[1]);
-                }
-            }
-        }
-
-        $url = $this->getView()->url(null, array(), array(), $reuseMatchedParams);
-        
-        if(strstr($url, '?')) {
-            $queryArray = array_merge($currentParams, $params);
-            return $this->getView()->url(null, $queryArray, array(), $reuseMatchedParams);;
-        }
-        
-        $queryString = http_build_query(array_merge($currentParams, $params));
-        return $this->getView()->url(null, array(), array(), $reuseMatchedParams) . '?' . $queryString;
+	/**
+	 *
+	 * @param array $params        	
+	 * @param bool $reset        	
+	 * @return string
+	 */
+	public function __invoke ($params = array(), $reuseMatchedParams = true, 
+			$resetCurrentParams = false)
+	{
+		$currentParams = array();
+		
+		if (! $resetCurrentParams) {
+			$queryString = urldecode($_SERVER['QUERY_STRING']);
+			parse_str($queryString, $currentParams);
+		}
+		
+		$url = $this->getView()->url(null, array(), array(), $reuseMatchedParams);
+		
+		if (strstr($url, '?')) {
+			$queryArray = array_merge($currentParams, $params);
+			return $this->getView()->url(null, $queryArray, array(), 
+					$reuseMatchedParams);
+			;
+		}
+		
+		$queryString = http_build_query(array_merge($currentParams, $params));
+		return $this->getView()->url(null, array(), array(), $reuseMatchedParams) . '?' . $queryString;
         
     }
 }
