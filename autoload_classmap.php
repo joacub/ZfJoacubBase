f9b6eea9f48e5cd161ada8f60c4ea1142c1f8f43
Johan <joacub@gmail.com>
2015-11-17 21:02:51 +0100

    session manager out
    Signed-off-by: Johan <joacub@gmail.com>

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -2,6 +2,5 @@
 $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 return array(
-    'Zend\\Session\\SessionManager' => $vendorDir . '/zf-joacub-base/src/JoacubBase/Session/SessionManager.php',
     'Zend\Cache\Storage\Adapter\\MemcachedResourceManager' => $vendorDir . '/zf-joacub-base/src/JoacubBase/Cache/Storage/Adapter/MemcachedResourceManager.php',
 );
