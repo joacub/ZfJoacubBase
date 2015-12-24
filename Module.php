9d5d083c28fd35b9495549428464eea6d97f762c
Johan <joacub@gmail.com>
2015-12-24 05:57:50 +0100

    cambios
    Signed-off-by: Johan <joacub@gmail.com>

--- Module.php
+++ Module.php
@@ -35,9 +35,6 @@ class Module implements ServiceProviderInterface
     public function getAutoloaderConfig()
     {
         return array(
-            'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
-            ),
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
--- autoload_classmap.php
+++
@@ -1,6 +0,0 @@
-<?php
-$vendorDir = dirname(dirname(__FILE__));
-$baseDir = dirname($vendorDir);
-return array(
-    'Zend\Cache\Storage\Adapter\\MemcachedResourceManager' => $vendorDir . '/zf-joacub-base/src/JoacubBase/Cache/Storage/Adapter/MemcachedResourceManager.php',
-);
